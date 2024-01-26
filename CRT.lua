44b9e4266822c131183d08737af683f6cf8bccb9
Bartek16194 <bartek16194@protonmail.com>
2024-01-26 01:03:19 +0100

    Moose integration, added a lot of stuff

--- CRT.lua
+++ CRT.lua
@@ -1,58 +1,82 @@
 --[[
 	Welcome to DCS-CRT (Carrier Recovery Tool)
 	
-	Tool allows for determining which CASE should be currently executed in the mission.
-
-	It takes into account factors such as:
-		-Time of day and 30-minute additional times before and after night 
-		-Cloud base 
-		-Visibility 
-
-	Due to DCS limitations:
-		-It allows CASE 2 when cloud coverage is less than 6/10.
-		-It does not allow the execution of CASE 1, despite potential visibility above 5 miles, due to the lack of a method to determine visibility during precipitation.
-
-	In game usage
-		Select 'Other' from the comms menu, and then choose 'CRT - What CASE?'
+	This tool allows for automatic detection of weather conditions, visibility, time of day/night, and based on that, it provides you with two options:
+		- Managment of MOOSE AIRBOSS, with this tool you can automaticaly set recowery window for any weather conditions (except dynamic weather due to DCS limitations). 
+		It also manages things like TACAN, ICLS, Wind Over Deck and other Airboss features see manual
+		- Displaying information on which CASE should currently be executed and the reason for this decision.
 
 	Installation
-		Prerequisites
-			[Only latest MOOSE](https://github.com/FlightControl-Master/MOOSE/releases)
+	### Prerequisites
+	[latest MOOSE](https://github.com/FlightControl-Master/MOOSE/releases)
 
-	Adding into mission
-		-First make sure MOOSE is loaded, either as an Initialization Script for the mission or the first DO SCRIPT with a "TIME MORE" of 1. "TIME MORE" means run the actions after X seconds into the mission.
-		-Load the CRT a few seconds after MOOSE using a second trigger with a "TIME MORE" and a DO SCRIPT of CRT.lua.
-		
+		Adding script into mission
+		- First make sure MOOSE is loaded.
+		- Load CRT.lua after MOOSE using a second trigger with a "TIME MORE" (**minimum 1 sec after moose**) and a DO SCRIPT of CRT.lua.		
+
+	Moose Airboss integration Setup
+
+	If you want to use integration with Moose Airboss, you need to set the at least two values in the CRT.lua, with the two most important ones being:
+	- `carrier_MOOSE_airboss` - boolean (true/false)
+	- `carrier_unit_name` - string (carrier unit name in DCS, NOT GROUP)
+	- Below that you can also set most common things in config section
+
+	**Remember if you set `carrier_MOOSE_airboss` to true you need to fill `carrier_unit_name`** 
+
+	Change other values or leave them at default. Reffer to [Moose guide](https://flightcontrol-master.github.io/MOOSE_DOCS_DEVELOP/Documentation/Ops.Airboss.html) for more. 
+	For more customization see function `carrier_on()`.
+
 	Contact / Support
 	If you need support or if you like to contribute, jump into my [Discord](https://discord.gg/yYs9HSq).
 
 	See https://github.com/Bartek16194/DCS-CRT-Carrier-Recovery-Tool for a user manual and the latest release.
 ]]
 
---VERSION: 1.0
+--VERSION: 2.0
+
+----[[ ##### SCRIPT CONFIGURATION ##### ]]----
+
+carrier_MOOSE_airboss = true
+
+carrier_unit_name = "Grupa CVN-71-1"
+carrier_airboss_name = "Roosevelt"
+carrier_morse_code = "ROS"
+carrier_tacan = 1
+carrier_tacan_mode = "X"
+carrier_icls = 1
+carrier_SetPatrolAdInfinitum = true
+carrier_SetMenuSmokeZones = false
+carrier_SetMenuMarkZones = false
+carrier_SetMarshalRadio = 127.500
+carrier_SetLSORadio = 127.500
+carrier_SetDefaultPlayerSkill = AIRBOSS.Difficulty.Easy
+carrier_SetAirbossNiceGuy = true
+
+
+carrier_holdingoffset = {-30,-15,0,15,30} --only in CASE 3
+carrier_turnintowind = true
+
+----[[ ##### End of SCRIPT CONFIGURATION ##### ]]----
 
 function IsNight()
 	local zero_point = COORDINATE:NewFromVec2({0,0})
-	local sunset = zero_point:GetSunset(true)
-	local sunrise = zero_point:GetSunrise(true)
+	local sunset = zero_point:GetSunset(true)+1800
+	local sunrise = zero_point:GetSunrise(true)-1800
 	local night 
 	
-	if (timer.getAbsTime() < sunrise+1800) then
+	if (timer.getAbsTime() > sunset) or (timer.getAbsTime() < sunrise) then --NOC 
 		night = true
-	end
-	if (timer.getAbsTime() > sunrise+1800) then
+	else
 		night = false
-	end
-	if (timer.getAbsTime() > sunset-1800) then
-		night = true
 	end	
-	
-return night
+	--MESSAGE:New("night = " .. tostring(night), 10):ToAll()
+return night,sunrise,sunset
 end
 
 function CloudInfo()
 local clouds = env.mission.weather.clouds
 local clouddens = clouds.density
+local temperature = env.mission.weather.season.temperature
 
 local cloudspreset = clouds.preset or "Nothing"
 
@@ -180,7 +204,7 @@ return precepitation,clouddens
 
 end 
 
-function weather_case_factor()
+function weather_case_factor(show_info)
     local weather = env.mission.weather
     --local visibility = UTILS.Round(weather.visibility.distance / 1852,2) --NM //broken
     local base = weather.clouds.base*3.281
@@ -195,7 +219,7 @@ function weather_case_factor()
 	
     local dynamic_weather = weather.atmosphere_type 
 	
-	if IsNight() == false and dynamic_weather == 0 then	-- is DAY and not dynamic_weather
+	if (select(1, IsNight()) == false and dynamic_weather == 0) then	-- is DAY and not dynamic_weather
 		REASON = "\n-Not night\n-Disabled dynamic_weather\n"
 		if precepitation == 0 or (fog_visibility > 5 and fog_visibility ~=0 ) then --no RAIN/SNOW and no FOG
 			REASON = REASON.."-No rain/snow\n-No fog\n"
@@ -229,11 +253,68 @@ function weather_case_factor()
 		REASON = "\n-Night or enabled dynamic_weather\n"
 	end
 	
+	if show_info == true then
 	MESSAGE:New(tostring("CASE: " .. CASE), 10):ToAll()
 	MESSAGE:New(tostring("REASON: " .. REASON), 10):ToAll()
+	end
 	
+	return CASE
 end
 
-weather_case_factor()
+function carrier_on()
+	local myAirboss=AIRBOSS:New(carrier_unit_name, carrier_airboss_name)
+	myAirboss:SetTACAN(carrier_tacan, carrier_tacan_mode, carrier_morse_code)
+	myAirboss:SetICLS(carrier_icls, carrier_morse_code)
+	myAirboss:SetPatrolAdInfinitum(carrier_SetPatrolAdInfinitum)
+	myAirboss:SetMenuSmokeZones(carrier_SetMenuSmokeZones)
+	myAirboss:SetMenuMarkZones(carrier_SetMenuMarkZones)
+	myAirboss:SetMarshalRadio(carrier_SetMarshalRadio)
+	myAirboss:SetLSORadio(carrier_SetLSORadio)
+	myAirboss:SetDefaultPlayerSkill(carrier_SetDefaultPlayerSkill)
+	myAirboss:SetAirbossNiceGuy(carrier_SetAirbossNiceGuy)
+	myAirboss:SetStaticWeather(env.mission.weather.atmosphere_type)
+	myAirboss:SetSoundfilesFolder("Airboss Soundfiles/")
+	myAirboss:Start()
+	recovery_scheduler(myAirboss)
+end
 
-MENU_MISSION_COMMAND:New("CRT - What CASE?", nil, weather_case_factor)
+function recovery_scheduler(carrier_instance)
+	local sunrise = tostring( UTILS.SecondsToClock(select(2, IsNight()), true) )
+	local sunset = tostring( UTILS.SecondsToClock(select(3, IsNight()), true) )
+	local current_time = tostring( UTILS.SecondsToClock(timer.getAbsTime(), true) )
+	local sunrise_raw = select(2, IsNight())
+	local sunset_raw = select(3, IsNight())
+
+		if current_time > sunrise and current_time < sunset then --after sunrise but before sunset
+			carrier_instance:AddRecoveryWindow(current_time,sunset, weather_case_factor(false), nil, carrier_turnintowind)
+			carrier_instance:AddRecoveryWindow(sunset,sunrise.."+1", 3, math.random(#carrier_holdingoffset), carrier_turnintowind)
+			
+			timer.scheduleFunction(recovery_scheduler, carrier_instance, (days_passed()*86400)+sunrise_raw)
+		elseif current_time > sunrise and current_time > sunset then --after sunrise and sunset
+			carrier_instance:AddRecoveryWindow(current_time,sunrise.."+1", 3, math.random(#carrier_holdingoffset), carrier_turnintowind)
+			carrier_instance:AddRecoveryWindow(sunrise.."+1",sunset.."+1", weather_case_factor(false), nil, carrier_turnintowind)
+			
+			timer.scheduleFunction(recovery_scheduler, carrier_instance, (days_passed()*86400)+sunset_raw)
+		else --before sunrise
+			carrier_instance:AddRecoveryWindow(current_time,sunrise, 3, math.random(#carrier_holdingoffset), carrier_turnintowind)
+			carrier_instance:AddRecoveryWindow(sunrise,sunset, weather_case_factor(false), nil, carrier_turnintowind)
+			carrier_instance:AddRecoveryWindow(sunset,sunrise.."+1", 3, math.random(#carrier_holdingoffset), carrier_turnintowind)
+			
+			timer.scheduleFunction(recovery_scheduler, carrier_instance, (days_passed()*86400)+sunrise_raw)
+		end
+end
+
+function days_passed()
+local day = math.floor(timer.getAbsTime() / 86400) +1
+return day
+end
+
+if carrier_MOOSE_airboss == true then
+	timer.scheduleFunction(carrier_on, nil, timer.getTime() + 1)
+	weather_case_factor(true)
+	MESSAGE:New("Carrier Recovery Tool - Managing carrier: "..carrier_unit_name.."\n if config is correct you should get new radio item in 'Others'", 30):ToAll()
+else
+	weather_case_factor(true)
+	MESSAGE:New("Carrier Recovery Tool - No carrier set in config\nTool will not manage any carrier and only will show what CASE should be conducted in mission.", 30):ToAll()
+end
+MENU_MISSION_COMMAND:New("CRT - What CASE?", nil, weather_case_factor,true)
